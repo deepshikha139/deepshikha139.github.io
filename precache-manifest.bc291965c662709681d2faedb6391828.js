f5a4ab4d5c1eb1bd294d75a545dc92a0c6fd53fb
Deepshikha Sharma <dsharma@techracers.io>
2020-06-22 03:09:51 +0530

    Updates

---
+++ precache-manifest.bc291965c662709681d2faedb6391828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e35353c517b25c15bd23df3278ce801",
+    "url": "/deepshikha139/deepshikha139/index.html"
+  },
+  {
+    "revision": "3990dcb2df1d3a2ef4c9",
+    "url": "/deepshikha139/deepshikha139/static/css/main.9e2e7fbf.chunk.css"
+  },
+  {
+    "revision": "b454df9df8851c0dafab",
+    "url": "/deepshikha139/deepshikha139/static/js/2.abe80afd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/deepshikha139/deepshikha139/static/js/2.abe80afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3990dcb2df1d3a2ef4c9",
+    "url": "/deepshikha139/deepshikha139/static/js/main.d2bcca15.chunk.js"
+  },
+  {
+    "revision": "a2b76b72c8e8c7c0aaec",
+    "url": "/deepshikha139/deepshikha139/static/js/runtime-main.42f688ba.js"
+  }
+]);
